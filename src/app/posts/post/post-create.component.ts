f92d839439b57e11db9ad88cc725e12d4221b313
Wilson <wilson@Wilsons-MacBook-Pro-2.local>
2020-05-06 21:59:51 -0400

    update

--- src/app/posts/post/post-create.component.ts
+++ src/app/posts/post/post-create.component.ts
@@ -5,5 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './post-create.component.html'
 })
 export class PostCreateComponent {
-
+    onAddComments() {
+      alert('Please double check your comments')
+    }
 }
