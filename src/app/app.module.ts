831904b76b26ebd03c097fbfebc9bd4d4b1c1365
Wilson <wilson@Wilsons-MacBook-Pro-2.local>
2020-05-07 14:15:38 -0400

    Responsive display what you input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { PostCreateComponent }  from './posts/post/post-create.component';
 import { HeaderComponent} from './header/header.component';
 import { PostListComponent } from './posts/list/post-list-component';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
--- src/app/posts/post/post-create.component.ts
+++ src/app/posts/post/post-create.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-post-component',
+  selector: 'app-post-create',
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css'],
 })
 export class PostCreateComponent {
-  newValue = '';
-  inputCommnets = 'Comment'
+  newPostTitle = '';
+  newPostContent = '';
+  @Output() createdPost = new EventEmitter();
+
   onAddComments() {
-    this.inputCommnets = this.newValue;
+    const post = {
+      title: this.newPostTitle,
+      content: this.newPostContent
+      };
+      this.createdPost.emit(post);
   }
 }
