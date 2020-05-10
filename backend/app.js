fa2e713f4d2e92d95dbd0a09d31c43ae8045781d
Wilson <wilson@Wilsons-MacBook-Pro-2.local>
2020-05-10 16:37:43 -0400

    update with refresh page still displaying content

--- backend/app.js
+++ backend/app.js
@@ -73,6 +73,15 @@ app.put("/api/posts/:id", (req, res, next) => {
   })
 });
 
+app.get("/api/posts/:id", (req, res, next) => {
+  Post.findById(req.params.id).then(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({message: "not found!"})
+    }
+  })
+})
 
 module.exports = app;
 
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -3,6 +3,7 @@ import { Subject } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { stringify } from 'querystring';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -54,13 +55,19 @@ export class PostsService {
   }
 
   getPost(id: string) {
-    return {...this.posts.find(p => p.id === id)};
+    return this.http.get<{_id: string, title: string, content: string}>("http://localhost:3000/api/posts/" +id);
   }
 
   updatePost(id: string, title: string, content: string) {
     const post: Post = {id: id, title: title, content: content};
     this.http.put("http://localhost:3000/api/posts/" +id, post)
-    .subscribe(response => console.log(response));
+    .subscribe(response => {
+      const updatedPosts = [...this.posts];
+      const oldPostIndex = updatedPosts.findIndex(p => p.id ===post.id);
+      updatedPosts[oldPostIndex] = post;
+      this.posts = updatedPosts;
+      this.postsUpdated.next([...this.posts]);
+    });
   }
 
 }
