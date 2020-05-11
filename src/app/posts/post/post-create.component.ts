5281edb597d43b5558f6d0740bad5ec4a51b79fa
Wilson <wilson@Wilsons-MacBook-Pro-2.local>
2020-05-10 23:11:08 -0400

    upload to frontend

--- src/app/posts/post/post-create.component.ts
+++ src/app/posts/post/post-create.component.ts
@@ -22,9 +22,10 @@ export class PostCreateComponent implements OnInit{
 
   ngOnInit() {
     this.form = new FormGroup({
-      'title': new FormControl(null,
+      title: new FormControl(null,
         {validators: [Validators.required, Validators.minLength(3)]}),
-      'content': new FormControl(null, {validators: [Validators.required]})
+      content: new FormControl(null, {validators: [Validators.required]}),
+      image: new FormControl(null, {validators: [Validators.required]})
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')) {
@@ -51,7 +52,11 @@ export class PostCreateComponent implements OnInit{
 
 
   onImagePicked(event: Event) {
-    const file = (event.target as HTMLInputElement).files
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+    console.log(file);
+    console.log(this.form);
   }
 
   onSaveComments() {
