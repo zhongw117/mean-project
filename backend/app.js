fd013ed9356955a307f44c5ba18e5eb62141bf89
Wilson <wilson@Wilsons-MacBook-Pro-2.local>
2020-05-10 22:44:38 -0400

    upload images

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use("/api/posts",postsRoutes);
+app.use("/api/posts", postsRoutes);
 
 module.exports = app;
 
--- src/app/posts/post/post-create.component.ts
+++ src/app/posts/post/post-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PostsService } from '../posts.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from '../post.model';
@@ -16,10 +16,16 @@ export class PostCreateComponent implements OnInit{
   private postId: string;
   post: Post;
   isLoading = false;
+  form: FormGroup;
 
   constructor(public postService: PostsService, public route: ActivatedRoute) {};
 
   ngOnInit() {
+    this.form = new FormGroup({
+      'title': new FormControl(null,
+        {validators: [Validators.required, Validators.minLength(3)]}),
+      'content': new FormControl(null, {validators: [Validators.required]})
+    });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')) {
         this.mode = 'edit';
@@ -27,7 +33,14 @@ export class PostCreateComponent implements OnInit{
         this.isLoading = true;
         this.postService.getPost(this.postId).subscribe(postData => {
           this.isLoading = false;
-          this.post = {id: postData._id, title: postData.title, content: postData.content};
+          this.post = {
+            id: postData._id,
+            title: postData.title,
+            content: postData.content};
+            this.form.setValue({
+              'title': this.post.title,
+              'content': this.post.content
+            });
         });
       } else {
         this.mode = 'create';
@@ -36,21 +49,26 @@ export class PostCreateComponent implements OnInit{
     });
   }
 
-  onSaveComments(form: NgForm) {
-    if (form.invalid) {
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files
+  }
+
+  onSaveComments() {
+    if (this.form.invalid) {
       return;
     }
     this.isLoading = true;
     if (this.mode ==="create") {
-      this.postService.addPost(form.value.title, form.value.content);
+      this.postService.addPost(this.form.value.title, this.form.value.content);
     } else {
       this.postService.updatePost(
         this.postId,
-        form.value.title,
-        form.value.content
+        this.form.value.title,
+        this.form.value.content
       );
     }
 
-    form.resetForm();
+    this.form.reset();
   }
 }
